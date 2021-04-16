e1766f07b2de960bb54ede1e2a1fe5400654540a
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-16 09:44:22 -0700

    update Person.test with ageMercury test --WIP:bad fail

--- __tests__/Person.test.js
+++ __tests__/Person.test.js
@@ -4,10 +4,16 @@ describe('Person', () => {
   let person;
   beforeEach(() => {
   person = new Person;
+  person.age = 41;
   });
 
   test('it should give the person an age of 45', () => {
   person.name = 45;
   expect(person.name).toEqual(45);
+  });
+
+  test('it should give the age of the person in "Mecury years"', () => {
+  person.age = person.ageMecury(person.age);
+  expect(person.age).toEqual(person.ageMecury(person.age));
   });
 });
