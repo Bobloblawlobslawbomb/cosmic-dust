0359eaaaa994bdd4cbcf1e694ad193e02680c1d7
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-16 12:04:06 -0700

    clarify first test which is not a real 'test'-/lunch

--- __tests__/Person.test.js
+++ __tests__/Person.test.js
@@ -3,17 +3,17 @@ import Person from './../src/Person.js';
 describe('Person', () => {
   let person;
   beforeEach(() => {
-  person = new Person;
-  person.name = 'testPerson'
-  person.age = 41;
+    person = new Person;
+    person.name = 'testPerson'
+    person.age = 41;
   });
 
-  test('it should give the person an age of 45', () => {
+test('this is a "test-test" to see if I have everything lined up correctly. It should give the person an age of 45', () => {
   person.age = 45;
   expect(person.age).toEqual(45);
   });
 
-  test('it should give the age of the person in "Mecury years"', () => {
+test('it should give the age of the person in "Mecury years"', () => {
   console.log(person.age);
   console.log((person.age*.24));//what i want .ageMerc todo
   expect(person.age).toEqual(9.84);
