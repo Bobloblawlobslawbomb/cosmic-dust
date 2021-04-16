25b0f61b27ee3b5d78a10bea2104493353a4ccd0
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-16 16:23:05 -0700

    WIP:more work on mercury test, also changed person var to personOne

--- __tests__/Person.test.js
+++ __tests__/Person.test.js
@@ -1,20 +1,20 @@
 import Person from './../src/Person.js';
 
 describe('Person', () => {
-  let person;
+  let personOne;
   beforeEach(() => {
-    person = new Person;
-    person.name = 'testPerson'
-    person.age = 41;
+    personOne = new Person;
+    personOne.name = 'Person One'
+    personOne.age = 41;
   });
 
 test('this is a "test-test" to see if I have everything lined up correctly. It should give the person an age of 45', () => {
-    person.age = 45;
-    expect(person.age).toEqual(45);
+  personOne.age = 45;
+  expect(personOne.age).toEqual(45);
   });
 
 test('it should multiply the persons age by .24 to give the persons age in "Mercury years"', () => {
-  person.ageMercury = person.ageMercury();
-  expect(person.ageMercury).toEqual(9.84);
+  personOne.ageMercury = personOne.ageMercury();
+  expect(personOne.ageMercury).toEqual(9.84);
   });
 });
