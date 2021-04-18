2b06323f072dfa7cf076309f54d011b0d9a64f8e
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-18 13:07:26 -0700

    remove 'classkeyword'format from person.js / update test with initial tests

--- __tests__/Person.test.js
+++ __tests__/Person.test.js
@@ -1,20 +1,16 @@
 import Person from './../src/Person.js';
 
 describe('Person', () => {
-  let personOne;
-  beforeEach(() => {
-    personOne = new Person;
-    personOne.name = 'Person One'
-    personOne.age = 41;
+  let testPerson;
+
+  test('it should correctly create a person object with a new name and age', () => {
+    testPerson = new Person('New Name', 35);
+    expect(testPerson.name).toEqual("New Name");
   });
 
-test('this is a "test-test" to see if I have everything lined up correctly. It should give the person an age of 45', () => {
-  personOne.age = 45;
-  expect(personOne.age).toEqual(45);
+  test('it should correctly create a person object with a new name and age', () => {
+    testPerson = new Person('New Name', 35);
+    expect(testPerson.age).toEqual(35);
   });
-
-test('it should multiply the persons age by .24 to give the persons age in "Mercury years"', () => {
-  personOne.ageMercury = personOne.ageMercury();
-  expect(personOne.ageMercury).toEqual(9.84);
-  });
+  
 });
--- src/person.js
+++ src/person.js
@@ -1,16 +1,34 @@
-export default class Person {
-  constructor (name, age){
-    this.name = name;
-    this.age = age;
-    this.avgLife = 100;
-    this.ageMercury = 0;
-    this.ageVenus = 0;
-    this.ageMars = 0;
-    this.ageJupiter = 0;
-    this.lifeExpectancy = 0;
-  }
-
-  ageMercury() {
-  //this.ageMercury = this.age * .24;
-  }
+export default function Person(name,age) {
+  this.name = name;
+  this.age = age;
+  this.avgLife = 100;
+  this.numOfSnakes = 0;
+  this.mercAge = 0;
+  this.venAge = 0;
+  this.marsAge = 0;
+  this.jupAge = 0;
+  this.lifeExpect = 0;
 }
+
+Person.prototype.ageMercury = function(){
+  
+}
+
+
+
+// export default class Person {
+//   constructor (name, age){
+//     this.name = name;
+//     this.age = age;
+//     this.avgLife = 100;
+//     this.ageMercury = 0;
+//     this.ageVenus = 0;
+//     this.ageMars = 0;
+//     this.ageJupiter = 0;
+//     this.lifeExpectancy = 0;
+//   }
+
+//   ageMercury() {
+//   //this.ageMercury = this.age * .24;
+//   }
+// }
