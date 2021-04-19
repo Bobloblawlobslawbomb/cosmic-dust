ab23de0c7155d325e7ef611e0634fd40f5895145
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-19 00:21:54 -0700

    update most functions to take a param/only 1 test fail!

--- __tests__/cosmic-dust.test.js
+++ __tests__/cosmic-dust.test.js
@@ -1,51 +1,51 @@
 import Person from './../src/cosmic-dust.js';
 
 describe('Person', () => {
-  let testPerson;
-  let personOne;
-  beforeEach(() =>{
-    personOne = new Person('Person One', 41, 1);
-  })
+    let testPerson;
+    let personOne;
+    beforeEach(() => {
+        personOne = new Person('Person One', 41, 1);
+    })
 
-  test('it should correctly create a test person object with a new name and age', () => {
-      testPerson = new Person('New Name', 35, 1);
-      expect(testPerson.name).toEqual("New Name");
-  });
+    test('it should correctly create a test person object with a new name and age', () => {
+        testPerson = new Person('New Name', 35, 1);
+        expect(testPerson.name).toEqual("New Name");
+    });
 
-  test('it should correctly create a test person object with a new name and age', () => {
-      testPerson = new Person('New Name', 35, 1);
-      expect(testPerson.age).toEqual(35);
-  });
+    test('it should correctly create a test person object with a new name and age', () => {
+        testPerson = new Person('New Name', 35, 1);
+        expect(testPerson.age).toEqual(35);
+    });
 
-  test('it should correctly determine personOnes "ageMercury" (age * .24)', () => {
-      expect(personOne.ageMercury()).toBeCloseTo(9.84);
-  });
+    test('it should correctly determine personOnes "ageMercury" (age * .24)', () => {
+        expect(personOne.ageMercury(personOne.age)).toBeCloseTo(9.84);
+    });
 
-  test('it should save a persons "ageMercury" as their mercAge property', () => {
-      //does this fall out of scope because person.js is not using class syntax? Or another reason?
-      expect(personOne.mercAge).toEqual(personOne.ageMercury());
-  });
+    test('it should save a persons "ageMercury" as their mercAge property', () => {
+        personOne.ageMercury(personOne.age);
+        expect(personOne.mercAge).toEqual(personOne.ageMercury(personOne.age));
+    });
 
-  test('it should correctly determine personOnes "ageVenus" (age * .62)', () => {
-      expect(personOne.ageVenus()).toBeCloseTo(25.42);
-  });
+    test('it should correctly determine personOnes "ageVenus" (age * .62)', () => {
+        expect(personOne.ageVenus(personOne.age)).toBeCloseTo(25.42);
+    });
 
-  test('it should correctly determine personOnes "ageMars" (age * 1.88)', () => {
-      expect(personOne.ageMars()).toBeCloseTo(77.08);
-  });
-  
-  test('it should correctly determine personOnes "ageJupiter" (age * 11.86)', () => {
-      expect(personOne.ageJupiter()).toBeCloseTo(486.26);
-  });
+    test('it should correctly determine personOnes "ageMars" (age * 1.88)', () => {
+        expect(personOne.ageMars(personOne.age)).toBeCloseTo(77.08);
+    });
 
-  test('it should determine a persons "lifeExpectancy" ((numOfSnakes * .2) * avgLife)', () => {
-      personOne.numOfSnakes = 3;
-      expect(personOne.lifeExpectancy()).toBeCloseTo(60);
-  });
+    test('it should correctly determine personOnes "ageJupiter" (age * 11.86)', () => {
+        expect(personOne.ageJupiter(personOne.age)).toBeCloseTo(486.26);
+    });
 
-  test('it should calculate personOnes "timeLeft" (lifeExpect - age)', () => {
-      //why is this not a function?
-      expect(personOne.timeLeft()).toBeCloseTo(59);
-  });
-  
+    test('it should determine a persons "lifeExpectancy" ((numOfSnakes * .2) * avgLife)', () => {
+        personOne.numOfSnakes = 3;
+        expect(personOne.lifeExpectancy()).toBeCloseTo(60);
+    });
+
+    test('it should calculate personOnes "timeLeft" (lifeExpect - age)', () => {
+        //why is this not a function?
+        expect(personOne.timeLeft()).toBeCloseTo(59);
+    });
+
 });
--- src/cosmic-dust.js
+++ src/cosmic-dust.js
@@ -1,4 +1,4 @@
-export default function Person(name,age, numOfSnakes) {
+export default function Person(name, age, numOfSnakes) {
   this.name = name;
   this.age = age;
   this.avgLife = 100;
@@ -11,32 +11,32 @@ export default function Person(name,age, numOfSnakes) {
   this.timeLeft = 100;
 }
 
-Person.prototype.ageMercury = function() {
-  this.mercAge = (this.age * .24);
+Person.prototype.ageMercury = function (ageParam) {
+  this.mercAge = parseFloat(ageParam * .24);
   return this.mercAge;
 }
 
-Person.prototype.ageVenus = function() {
-  this.venAge = (this.age * .62);
+Person.prototype.ageVenus = function (ageParam) {
+  this.venAge = parseFloat(ageParam * .62);
   return this.venAge;
 }
 
-Person.prototype.ageMars = function() {
-  this.marsAge = (this.age * 1.88);
+Person.prototype.ageMars = function (ageParam) {
+  this.marsAge = parseFloat(ageParam * 1.88);
   return this.marsAge;
 }
 
-Person.prototype.ageJupiter = function() {
-  this.jupAge = (this.age * 11.86);
+Person.prototype.ageJupiter = function (ageParam) {
+  this.jupAge = parseFloat(ageParam * 11.86);
   return this.jupAge;
 }
 
-Person.prototype.lifeExpectancy = function() {
+Person.prototype.lifeExpectancy = function () {
   this.lifeExpect = ((this.numOfSnakes * .2) * this.avgLife)
   return this.lifeExpect;
 }
 
-Person.prototype.timeLeft = function() {
+Person.prototype.timeLeft = function () {
   //why is this not a function?
   // this.timeLeft = (this.lifeExpect - this.age);
   // return this.timeLeft;
