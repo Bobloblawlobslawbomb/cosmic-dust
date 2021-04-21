4f219f2e1082be253d075ba6a951e96be9f9346b
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-20 23:19:04 -0700

    update class-syntax branch with class syntax code

--- src/cosmic-dust.js
+++ src/cosmic-dust.js
@@ -1,4 +1,5 @@
-export default function Person(name, age, numOfSnakes) {
+export default class Person {
+  constructor(name, age, numOfSnakes) {
   this.name = name;
   this.age = age;
   this.avgLife = 100;
@@ -9,35 +10,36 @@ export default function Person(name, age, numOfSnakes) {
   this.jupAge = 0;
   this.lifeExpect = 100;
   this.timeLeft = 100;
-}
+  }
 
-Person.prototype.ageMercury = function (ageParam) {
-  this.mercAge = parseFloat(ageParam * .24);
-  return this.mercAge;
-}
+  ageMercury(ageParam) {
+    this.mercAge = parseFloat(ageParam * .24);
+    return this.mercAge;
+    }
 
-Person.prototype.ageVenus = function (ageParam) {
-  this.venAge = parseFloat(ageParam * .62);
-  return this.venAge;
-}
+  ageVenus(ageParam) {
+    this.venAge = parseFloat(ageParam * .62);
+    return this.venAge;
+    }
 
-Person.prototype.ageMars = function (ageParam) {
-  this.marsAge = parseFloat(ageParam * 1.88);
-  return this.marsAge;
-}
+  ageMars(ageParam) {
+    this.marsAge = parseFloat(ageParam * 1.88);
+    return this.marsAge;
+    }
 
-Person.prototype.ageJupiter = function (ageParam) {
-  this.jupAge = parseFloat(ageParam * 11.86);
-  return this.jupAge;
-}
+  ageJupiter(ageParam) {
+    this.jupAge = parseFloat(ageParam * 11.86);
+    return this.jupAge;
+    }
 
-Person.prototype.lifeExpectancy = function () {
-  this.lifeExpect = ((this.numOfSnakes * .2) * this.avgLife)
-  return this.lifeExpect;
-}
+  lifeExpectancy() {
+    this.lifeExpect = ((this.numOfSnakes * .2) * this.avgLife)
+    return this.lifeExpect;
+    }
 
-Person.prototype.timeLeft = function () {
-  //why is this not a function?
-  // this.timeLeft = (this.lifeExpect - this.age);
-  // return this.timeLeft;
+  timeLeft() {
+    //why is this not a function?
+    // this.timeLeft = (this.lifeExpect - this.age);
+    // return this.timeLeft;
+    }
 }
