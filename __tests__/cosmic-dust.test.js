d6c037e52bd73dcddc02c72822f26b3e0e09556c
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-22 17:26:34 -0700

    update tests with better indentation and lifeLeft test

--- __tests__/cosmic-dust.test.js
+++ __tests__/cosmic-dust.test.js
@@ -1,56 +1,55 @@
 import Person from './../src/cosmic-dust.js';
 
 describe('Person', () => {
-    let testPerson;
-    let personOne;
-    beforeEach(() => {
-        personOne = new Person('Person One', 41, 1);
-    })
+  let testPerson;
+  let personOne;
+  beforeEach(() => {
+    personOne = new Person('Person One', 41, 1);
+  })
 
-    test('it should correctly create a test person object with a new name and age', () => {
-        testPerson = new Person('New Name', 35, 1);
-        expect(testPerson.name).toEqual("New Name");
-    });
+test('it should correctly create a test person object with a new name and age', () => {
+    testPerson = new Person('New Name', 35, 1);
+    expect(testPerson.name).toEqual("New Name");
+  });
 
-    test('it should correctly create a test person object with a new name and age', () => {
-        testPerson = new Person('New Name', 35, 1);
-        expect(testPerson.age).toEqual(35);
-    })
+test('it should correctly create a test person object with a new name and age', () => {
+    testPerson = new Person('New Name', 35, 1);
+    expect(testPerson.age).toEqual(35);
+  })
 
-    test('it should correctly determine personOnes "ageMercury" (age / .24)', () => {
-        expect(personOne.ageMercury(personOne.age)).toBeCloseTo(170.83);
-    });
+test('it should correctly determine personOnes "ageMercury" (age / .24)', () => {
+    expect(personOne.ageMercury(personOne.age)).toBeCloseTo(170.83);
+  });
 
-    test('it should save a persons "ageMercury" as their mercAge property', () => {
-        personOne.ageMercury(personOne.age);
-        expect(personOne.mercAge).toEqual(personOne.ageMercury(personOne.age));
-    });
+test('it should save a persons "ageMercury" as their mercAge property', () => {
+    personOne.ageMercury(personOne.age);
+    expect(personOne.mercAge).toEqual(personOne.ageMercury(personOne.age));
+  });
 
-    test('it should correctly determine personOnes "ageVenus" (age / .62)', () => {
-        expect(personOne.ageVenus(personOne.age)).toBeCloseTo(66.13);
-    });
+test('it should correctly determine personOnes "ageVenus" (age / .62)', () => {
+    expect(personOne.ageVenus(personOne.age)).toBeCloseTo(66.13);
+  });
 
-    test('it should save a persons "ageVenus" as their venAge property', () => {
-        personOne.ageVenus(personOne.age);
-        expect(personOne.venAge).toEqual(personOne.ageVenus(personOne.age));
-    });
+test('it should save a persons "ageVenus" as their venAge property', () => {
+    personOne.ageVenus(personOne.age);
+    expect(personOne.venAge).toEqual(personOne.ageVenus(personOne.age));
+  });
 
-    test('it should correctly determine personOnes "ageMars" (age / 1.88)', () => {
-        expect(personOne.ageMars(personOne.age)).toBeCloseTo(21.81);
-    });
+test('it should correctly determine personOnes "ageMars" (age / 1.88)', () => {
+    expect(personOne.ageMars(personOne.age)).toBeCloseTo(21.81);
+  });
 
-    test('it should correctly determine personOnes "ageJupiter" (age / 11.86)', () => {
-        expect(personOne.ageJupiter(personOne.age)).toBeCloseTo(3.46);
-    });
+test('it should correctly determine personOnes "ageJupiter" (age / 11.86)', () => {
+    expect(personOne.ageJupiter(personOne.age)).toBeCloseTo(3.46);
+  });
 
-    test('it should determine a persons "lifeExpectancy" ((numOfSnakes * .2) * avgLife)', () => {
-        personOne.numOfSnakes = 3;
-        expect(personOne.lifeExpectancy()).toBeCloseTo(60);
-    });
+test('it should determine a persons "lifeExpectancy" ((numOfSnakes * .2) * avgLife)', () => {
+    personOne.numOfSnakes = 3;
+    expect(personOne.lifeExpectancy()).toBeCloseTo(60);
+  });
 
-    test('it should calculate personOnes "timeLeft" (lifeExpect - age)', () => {
-        
-        expect(personOne.timeLeft()).toBeCloseTo(59);
-    });
-
+ test('it should calculate personOnes "lifeLeft" (lifeExpect - age)', () => {
+    
+    expect(personOne.lifeLeft()).toBeCloseTo(59);
+  });
 });
